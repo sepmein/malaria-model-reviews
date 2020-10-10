d47e80903fca752ae074451a034545527ff986c6
Spencer ZHANG <sepmein@gmail.com>
2020-10-10 17:13:23 +0200

    added apollo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,40 +1,24 @@
 import React from "react";
 import { Layout, Menu } from "antd";
 import "./App.css";
-import fetchGraphQL from './fetchGraphQL';
+import { gql, useQuery } from "@apollo/client";
 
 const { Header, Footer, Sider, Content } = Layout;
-const { useState, useEffect } = React
+const PAPERS = gql`
+  query GetPapers {
+    papers(order_by: { created_at: desc }) {
+      id
+      publish_at
+      title
+      updated_at
+      url
+      created_at
+    }
+  }
+`;
 
 function App() {
-  const [name, setName] = useState(null);
-  // When the component mounts we'll fetch a repository name
-  useEffect(() => {
-    let isMounted = true;
-    fetchGraphQL(`
-      query RepositoryNameQuery {
-        # feel free to change owner/name here
-        repository(owner: "facebook" name: "relay") {
-          name
-        }
-      }
-    `).then(response => {
-      // Avoid updating state if the component unmounted before the fetch completes
-      if (!isMounted) {
-        return;
-      }
-      const data = response.data;
-      setName(data.repository.name);
-    }).catch(error => {
-      console.error(error);
-    });
-
-    return () => {
-      isMounted = false;
-    };
-  }, [fetchGraphQL]);
-
-
+  const { loading, error, data } = useQuery(PAPERS);
   return (
     <Layout>
       <Header>
@@ -47,7 +31,6 @@ function App() {
       </Header>
       <Content>
         <div className="site-layout-content">Content</div>
-        <p>{name != null?`Repository: ${name}`: "Loading"}</p>
       </Content>
       <Footer>Design and created by zhangc@who.int</Footer>
     </Layout>
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import {
+  ApolloClient,
+  InMemoryCache,
+  gql,
+  ApolloProvider,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://famous-fowl-54.hasura.app/v1/graphql",
+  cache: new InMemoryCache(),
+});
+client
+  .query({
+    query: gql`
+      query MyQuery {
+        papers(order_by: { created_at: desc }) {
+          id
+          publish_at
+          title
+          updated_at
+          url
+          created_at
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
